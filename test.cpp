7e0c3a9c9f200bf7797822560ae1fad16f08a964
Alla eddine BOUMEDIENE <alboumed@e2r1p12.clusters.42paris.fr>
2023-04-06 16:56:17 +0200

    sdlkfjdslfkjd

--- test.cpp
+++ test.cpp
@@ -1,15 +1,23 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <iostream>
+#include <netinet/in.h>
+// #include <iomanip>
+// #include <stdio.h>
 // #include <stdint.h>
 
 typedef struct sockaddr t_sockaddr;
 
 int main()
 {
-	uint16_t a = 25;
-	uint8_t *p;
-	p = (uint8_t *)&a;
-	std::cout << p[0] << std::endl;
-	std::cout << p[1] << std::endl;
+	int fd_socket;
+	t_sockaddr socket_struct;
+
+	if ((fd_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1)
+		std::cerr << "socket creation error" << std::endl;
+	if (bind(fd_socket, &socket_struct, sizeof(socket_struct)) == -1)
+		std::cerr << "socket binding failed" << std::endl;
+	if (listen(fd_socket, 2) == -1)
+		std::cerr << "socket listening failed" << std::endl;
+	return (0);
 }
