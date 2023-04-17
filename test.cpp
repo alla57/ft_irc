2230e239996023d9479ae41c762b8a3751c63b20
Alla eddine BOUMEDIENE <alboumed@e1r3p16.clusters.42paris.fr>
2023-04-17 17:58:25 +0200

    sdfd fsdf dsf

--- test.cpp
+++ test.cpp
@@ -2,6 +2,8 @@
 #include <sys/socket.h>
 #include <iostream>
 #include <netinet/in.h>
+#include <unistd.h>
+#include <sys/wait.h>
 // #include <iomanip>
 // #include <stdio.h>
 // #include <stdint.h>
@@ -11,6 +13,7 @@ typedef struct sockaddr t_sockaddr;
 int main()
 {
 	int fd_socket;
+	int status;
 	t_sockaddr socket_struct;
 
 	if ((fd_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1)
@@ -19,5 +22,12 @@ int main()
 		std::cerr << "socket binding failed" << std::endl;
 	if (listen(fd_socket, 2) == -1)
 		std::cerr << "socket listening failed" << std::endl;
+	if (fork() == 0)
+	{
+		std::cout << "i am the child" << std::endl;
+		
+	}
+	else
+		wait(&status);
 	return (0);
 }
